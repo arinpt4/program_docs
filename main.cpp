44436ce11397d26b5b70769799c68c3df2f14cf1
arinpt4 <arinpt4@gmail.com>
2026-06-15 18:00:25 -0700

    deleted "Loading system data" cout to make it cleaner

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int main() {
     std::string inputFileName = "inputFile.txt";
     
     // 1. Load Data
-    std::cout << "Loading system data...\n";
     readSongData(hash, bst, inputFileName);
 
     // 2. Launch the Menu Loop (from ScreenManager)
