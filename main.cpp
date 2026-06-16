28b8c47caf92a4daacaeb76a97f65e5b5d646fb9
arinpt4 <arinpt4@gmail.com>
2026-06-15 21:13:45 -0700

    added printInfo() to main

--- main.cpp
+++ main.cpp
@@ -8,23 +8,34 @@ Name: Arin Thakkar
 #include "Screen Output/ScreenManager.h"
 #include "Screen Output/Stack.h"
 
+void printInfo();
+
 int main() {
     HashTable<Song>* hash = nullptr;
     BST bst;
     Stack undoStack;
     std::string inputFileName = "inputFile.txt";
+
+    // 1. Print Info
+    printInfo();
     
-    // 1. Load Data
+    // 2. Load Data
     readSongData(hash, bst, inputFileName);
 
-    // 2. Launch the Menu Loop (from ScreenManager)
+    // 3. Launch the Menu Loop (from ScreenManager)
     if (hash != nullptr) {
         runMenu(*hash, bst, undoStack);
     } else {
         std::cout << "Failed to initialize the system. Exiting.\n";
     }
 
-    // 3. Clean up
+    // 4. Clean up
     delete hash;
     return 0;
+}
+
+void printInfo(){
+    std::cout << "*******************************************************************************************************" << std::endl;
+    std::cout << "Welcome to the Song Player! The Song Player allows you to insert, delete, and view songs within a file." << std::endl;
+    std::cout << "*******************************************************************************************************" << std::endl;
 }
